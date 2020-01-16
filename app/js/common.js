465fa72713d47e9c39693eff91b61eca6e7f47f6
eugenejs <57704927+delictt@users.noreply.github.com>
2020-01-16 08:36:33 +0300

    services

--- app/js/common.js
+++ app/js/common.js
@@ -25,6 +25,28 @@ $( document ).ready(function() {
 		swipe: false
 	});
 
+	$('.services').slick({
+		dots: true,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [
+		{
+		breakpoint: 992,
+		settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2,
+		}
+		},
+		{
+		breakpoint: 768,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+		}
+		},
+		]
+	});
 
 
 });
