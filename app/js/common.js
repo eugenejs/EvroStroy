3ad4bfdc052a869efacc0e87faffb17898ab6ed3
eugenejs <57704927+delictt@users.noreply.github.com>
2020-01-21 13:36:23 +0300

    footer

--- app/js/common.js
+++ app/js/common.js
@@ -119,6 +119,14 @@ $( document ).ready(function() {
 		}
 	});
 
+    ymaps.ready(init);
+    function init(){
+        var myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 15
+        });
+    }
+
 
 
 
