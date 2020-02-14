0d8b340ea3bb43efb264f8e528d9330416869019
eugenejs <57704927+delictt@users.noreply.github.com>
2020-02-14 15:27:42 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -59,25 +59,18 @@ const pngquant = require('imagemin-pngquant')
 
       var csslibs = [
 		'app/libs/normalize.css/normalize.css',
-		// 'app/libs/bootstrap/dist/css/bootstrap-grid.min.css',
-		// 'app/libs/animate.css/animate.min.css',
 		'app/libs/hamburgers/dist/hamburgers.min.css',
-		// 'app/libs/owl.carousel/dist/assets/owl.carousel.min.css',
 		'app/libs/magnific-popup/dist/magnific-popup.css',
 		'app/libs/slick-slider/slick/slick.css',
-		'app/libs/slick-slider/slick/slick-theme.css',
+		'app/libs/slick-slider/slick/slick-theme.css'
       ]
 
     // js libs
 
       var jslibs = [
         'app/libs/jquery/dist/jquery.min.js',
-        // 'app/libs/owl.carousel/dist/owl.carousel.min.js',
-        // 'app/libs/wowjs/dist/wow.min.js',
-        // 'app/libs/scroll.js',
         'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
-		    'app/libs/slick-slider/slick/slick.min.js',
-        // 'app/libs/mixitup/dist/mixitup.min.js',
+		    'app/libs/slick-slider/slick/slick.min.js'
       ]
 
     // pipe libs
